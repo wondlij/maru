663676f244bc1585f8b4265559137998b10c1f01
Maru <mareshetabebe@outlook.com>
2020-02-18 07:25:42 -0800

    it is test

---
+++ demo/src/home.js
@@ -0,0 +1,16 @@
+{/*import React from 'react'
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Hello {this.props.name}
+          <p>
+            
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+export default Home*/}
