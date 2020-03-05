fa92488754c722a2966d27cce99b8d6b8467349f
Maru <mareshetabebe@outlook.com>
2020-03-05 15:23:02 -0800

    sprint1-feature/TAMS-100-MARU

--- demo/src/dashboard.js
+++ demo/src/dashboard.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import Hello from './hello'
+
 class Dashboard extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          Dashboard is in progress....
-        
+          Dashboard is in progress.... maru
+       
             
             <Hello name="Maru"></Hello>
-        
+        <div style ={{marginTop: "20px"}}>
+          <div className="d-flex flex-row">
+            <button className='btn btn-primary mr-2'>Kaleb</button>
+            <button className='btn btn-primary mr-2'>maru</button>
+          </div>
+
+        </div>
         </header>
       </div>
+      
     );
   }
 }
